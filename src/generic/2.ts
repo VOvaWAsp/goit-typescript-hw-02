a6ead54e080daaa3887894df1a6c732bb8e98d4d
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2024-03-11 22:21:36 -0500

    finall

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -13,7 +13,10 @@ type AllType = {
 
 type AllTypes = Pick<AllType, "name" | "position" | "color" | "weight">;
 
-function compare(top: AllTypes, bottom: AllTypes): AllTypes {
+function compare<
+  T extends Pick<AllType, "name" | "color">,
+  U extends Pick<AllType, "position" | "weight">
+>(top: T, bottom: U): AllTypes {
   return {
     name: top.name,
     color: top.color,
